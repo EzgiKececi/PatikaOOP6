5d135a3aff973b4e2e360fd3aa6698238b014cd1
Ezgi Kececi Atalay <ezgikececii@gmail.com>
2024-08-12 20:47:34 +0300

    Add files via upload

---
+++ PatikaPolymorphism/PatikaPolymorphism/BaseGeoShapes.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace PatikaPolymorphism
+{
+    public abstract class BaseGeoShapes
+    {
+        public double Width { get; set; }
+        public double Heigth { get; set; }
+
+        public virtual double AreaCalculation()
+        {
+            return Width * Heigth;
+    
+        }
+        
+    }
+
+    //Türetilmiş kares sınıfı
+    public class Square : BaseGeoShapes
+    {
+        public override double AreaCalculation()
+        {
+            return Width * Heigth;
+        } 
+
+    }
+
+    //Türetilmiş dikdörtgen sınıfı
+    public class Rectangle : BaseGeoShapes
+    {
+        public override double AreaCalculation()
+        {
+            return Width * Heigth;
+        }
+
+    }
+    //Türetilmiş üçgen sınıfı
+    public class RightTriangle : BaseGeoShapes
+    {
+        public override double AreaCalculation()
+        {
+            return (Width * Heigth)/2 ;
+        }
+
+    }
+
+}
+ 
+  
+        
+         
+    
+
+
---
+++ PatikaPolymorphism/PatikaPolymorphism/Program.cs
@@ -0,0 +1,22 @@
+﻿using PatikaPolymorphism;
+
+public class Program
+{
+    public static void Main()
+    {
+        Square square = new Square(); //Kare nesnesi oluşturma
+        square.Width = 10;
+        square.Heigth = 10;
+        Console.WriteLine($"Karenin alanı: {square.AreaCalculation()}");
+
+        Rectangle rectangle = new Rectangle(); //Dikdörtgen nesnesi oluşturma
+        rectangle.Width = 21;
+        rectangle.Heigth = 5;
+        Console.WriteLine($"Dikdörtgenin alanı: {rectangle.AreaCalculation()}");
+
+        RightTriangle rightTriangle = new RightTriangle(); //Üçgen nesnesi oluşturma
+        rightTriangle.Width = 10;
+        rightTriangle.Heigth = 8;
+        Console.WriteLine($"Üçgenin alanı: {rightTriangle.AreaCalculation()}");
+    }
+}
